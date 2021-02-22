35e5a4630b7253067e441dec3fb8dcc5ff857b78
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-02-22 18:20:50 +0600

    Actualize source according last django lesson

--- hello/webapp/urls.py
+++ hello/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view, home_view
+from webapp.views import index_view, home_view, article_create_view
 
 urlpatterns = [
     path('hello/', index_view),
     path('', home_view),
+    path('add/', article_create_view)
 ]
--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index_view(request):
     return render(request, 'index.html')
 
 
 def home_view(request):
     return render(request, 'home.html')
+
+
+def article_create_view(request):
+    if request.method == "GET":
+        return render(request, 'article_create.html')
+    elif request.method == "POST":
+        user = {"name": 'Vasya', "email": "vasya@mail.ru", "age": 25}
+        context = {
+            'title': request.POST.get("title"),
+            'content': request.POST.get("content"),
+            'author': request.POST.get("author"),
+            'user': user
+        }
+        return render(request, 'article_view.html', {
+            'title': request.POST.get("title"),
+            'content': request.POST.get("content"),
+            'author': request.POST.get("author"),
+            'user': user
+        })
