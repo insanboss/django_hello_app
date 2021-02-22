c8e9d33846170620824d20dc25109c0f439a11bf
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-02-22 19:29:59 +0600

    Создана модель Articles

--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=120, null=False, blank=False)
+    content = models.TextField(max_length=3000, null=False, blank=False)
+    author = models.CharField(max_length=150, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'articles'
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return f'{self.id}. {self.author}: {self.title}'
