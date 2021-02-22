92043c7aac9d3b4f209b984a338f71fb9b5ab65b
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-02-22 20:43:24 +0600

    Модель Article зарегистрирована в панели администратора

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Article
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'created_at', 'updated_at']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    fields = ['id', 'title', 'author', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+admin.site.register(Article, ArticleAdmin)
