5cf5febf9c03510d87184cdc55878d77959f76b8
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-02-26 13:43:19 +0600

    Обновлены файлы URLS:
    * Все url в приложении articles вынесены в пространство имён articles/
    * добавлено перенаправление на страницу /articles/ при переходе на
      главную страницу

--- hello/article/urls.py
+++ hello/article/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from article.views import index_view, article_view, article_create_view
 
 urlpatterns = [
-    path('', index_view),  # URL для отображения списка статей
-    path('articles/add/', article_create_view),  # URL для создания статьи
-    path('article/', article_view)  # URL для просмотра деталей статьи
+    path('', index_view, name='article-list'),  # URL для отображения списка статей
+    path('add/', article_create_view, name='article-add'),  # URL для отображения формы и создания статьи
+    path('<int:pk>/', article_view, name='article-view')  # URL для просмотра деталей статьи. Обратите внимание, URL использует целочисленный параметр id
 ]
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+
+HOMEPAGE_URL = 'articles/'  # константа для хранения домашней страницы. на данную страницу будет произведён редирект, когда пользователь зайдёт по пути /
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('article.urls'))
-]
+    path('articles/', include('article.urls')),  # подключаем URLs из приложения article
+    path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=True)),  # перенаправляем на страницу просмотра списка статей
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # подключаем ULR для статики
