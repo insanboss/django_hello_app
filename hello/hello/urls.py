01f2ff4cfa54c4f52eb7f1a25971b0c9a4b81599
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-02-26 17:11:08 +0600

    Подключены статические файлы стилей

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -27,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('article.urls')),  # подключаем URLs из приложения article
     path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=True)),  # перенаправляем на страницу просмотра списка статей
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # подключаем ULR для статики
+]
