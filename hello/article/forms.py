c896edffe7a4855fa275794ef261a588d21bf5d6
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-03 20:19:29 +0600

    Форма для создания и редактирвоания статей переделана на ModelForm

--- hello/article/forms.py
+++ hello/article/forms.py
@@ -1,17 +1,17 @@
 from django import forms
-from django.forms import widgets
+
+from article.models import Article
 
 
-class ArticleForm(forms.Form):
+class ArticleForm(forms.ModelForm):
     """
     Форма для создания и редактирваония объектов статьи
     https://docs.djangoproject.com/en/3.1/ref/forms/
     """
-    title = forms.CharField(max_length=120, required=True, label='Заголовок')
-    content = forms.CharField(max_length=3000, required=True, widget=widgets.Textarea, label='Контент')
-    author = forms.CharField(max_length=150, required=True, label='Автор')
+    class Meta:
+        model = Article
+        fields = ('title', 'content', 'author')
 
 
 class ArticleDeleteForm(forms.Form):
     title = forms.CharField(max_length=120, required=True, label='Введите название статьи, чтобы удалить её')
-
