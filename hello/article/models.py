a75e52971a374283d026378fee32d01e3c7abbd2
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-10 14:01:07 +0600

    Добавлена модель комментариев, связанная связью один-ко-многим с моделью статей (одна статья может иметь один комментарий)

--- hello/article/models.py
+++ hello/article/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 
 
-class Article(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Article(BaseModel):
     title = models.CharField(max_length=120, null=False, blank=False, verbose_name='Заголовок')
     content = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Контент')
     author = models.CharField(max_length=150, null=False, blank=False, default='Anon', verbose_name='Автор')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'articles'
@@ -17,3 +23,21 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.id}. {self.author}: {self.title}'
+
+
+class Comment(BaseModel):
+    article = models.ForeignKey(
+        'article.Article',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Статья',
+        null=False,
+        blank=False
+    )
+    comment = models.CharField(max_length=200, verbose_name='Комментарий', null=False, blank=False)
+    author = models.CharField(max_length=150, null=False, blank=False, verbose_name='Автор')
+
+    class Meta:
+        db_table = 'comments'
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
