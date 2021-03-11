cf9af473358f04c61f23c0a236393f8f8973c281
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-12 00:10:02 +0600

    Создана модель Tag, добавлена связь многие-ко-многим с моделью статей через промежуточную модель ArticleTag

--- hello/article/models.py
+++ hello/article/models.py
@@ -15,6 +15,12 @@ class Article(BaseModel):
     title = models.CharField(max_length=120, null=False, blank=False, verbose_name='Заголовок')
     content = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Контент')
     author = models.CharField(max_length=150, null=False, blank=False, default='Anon', verbose_name='Автор')
+    tags = models.ManyToManyField(
+        'article.Tag',
+        related_name='articles',
+        through='article.ArticleTag',
+        through_fields=('article', 'tag')
+    )
 
     class Meta:
         db_table = 'articles'
@@ -41,3 +47,25 @@ class Comment(BaseModel):
         db_table = 'comments'
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
+
+
+
+class Tag(BaseModel):
+    tag = models.CharField(max_length=200, verbose_name='Тэг')
+
+    class Meta:
+        db_table = 'tags'
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+    
+    def __str__(self):
+        return self.tag
+
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey('article.Article', on_delete=models.CASCADE)
+    article_old = models.ForeignKey('article.Article', on_delete=models.CASCADE, related_name='old_tags', null=True)
+    tag = models.ForeignKey('article.Tag', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'articel_tags'
