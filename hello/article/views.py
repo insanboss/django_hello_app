ca80819ed6c28ebc214f6242dd7d8a485a489cba
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-22 22:22:45 +0600

    Представление для отображение списка статей переделано с использованием generic-представления ListView, добавлен поиск и пагинация

--- hello/article/views.py
+++ hello/article/views.py
@@ -1,27 +1,58 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, ListView
 from django.urls import reverse
+from django.db.models import Q
+from django.utils.http import urlencode
 
 from article.models import Article
-from article.forms import ArticleForm, ArticleDeleteForm
+from article.forms import ArticleForm, ArticleDeleteForm, SearchForm
+
 from article.base_views import CustomFormView
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     """
     Представление для просмотра списка статей. Представление реализовано с
-    использованием generic-представления TemplateView.
+    использованием generic-представления ListView.
 
-    Поскольку метод get уже реализован в TemplateView, нам остаётся только указать
-    название шаблона, который мы будем использовать в свойстве класса template_name и
-    переопределить метод получения контекста get_context_data, для того, чтобы передать
-    QuerySet со статьями в шаблон
+    В представлении активирована пагинация и реализован поиск
     """
     template_name = 'index.html'
+    model = Article
+    context_object_name = 'articles'
+    ordering = ('title', '-created_at')
+    paginate_by = 5
+    paginate_orphans = 1
 
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(IndexView, self).get(request, **kwargs)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(title__icontains=self.search_data) |
+                Q(author__icontains=self.search_data) |
+                Q(content__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+    
     def get_context_data(self, **kwargs):
-        kwargs['articles'] = Article.objects.all()
-        return super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+
+        if self.search_data:
+            context['query'] = urlencode({'search_value': self.search_data})
+
+        return context
 
 
 class ArticleView(TemplateView):
@@ -35,8 +66,6 @@ class ArticleView(TemplateView):
     template_name = 'article_view.html'
 
     def get_context_data(self, **kwargs):
-        print(kwargs, '\n\n\n')
-        print(self.kwargs, '\n\n\n')
         kwargs['article'] = get_object_or_404(Article, id=kwargs.get('pk'))
         return super().get_context_data(**kwargs)
 
