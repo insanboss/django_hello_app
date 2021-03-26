8e2a04d0ede3bbc39d87477359aea0d7db3470c1
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-26 12:39:48 +0600

    Добавлен url для создания комментариев

--- hello/article/urls.py
+++ hello/article/urls.py
@@ -5,13 +5,15 @@ from article.views import (
     ArticleView,
     article_delete_view,
     CreateArticleView,
-    ArticleUpdateView
+    ArticleUpdateView,
+    ArticleCommentCreate
 )
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='article-list'),  # URL для отображения списка статей
-    path('add/', CreateArticleView.as_view(), name='article-add'),  # URL для отображения формы и создания статьи
-    path('<int:pk>/', ArticleView.as_view(), name='article-view'),  # URL для просмотра деталей статьи. Обратите внимание, URL использует целочисленный параметр id
-    path('<int:pk>/update', ArticleUpdateView.as_view(), name='article-update'),  # URL для отображения формы и редактирования статьи
-    path('<int:pk>/delete', article_delete_view, name='article-delete')  # URL для отображения формы подтверждения и удаления статьи
+    path('', IndexView.as_view(), name='article-list'),
+    path('add/', CreateArticleView.as_view(), name='article-add'),
+    path('<int:pk>/', ArticleView.as_view(), name='article-view'),
+    path('<int:pk>/update', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:pk>/delete', article_delete_view, name='article-delete'),
+    path('<int:pk>/commnets/add/', ArticleCommentCreate.as_view(), name='article-commnet-create')
 ]
