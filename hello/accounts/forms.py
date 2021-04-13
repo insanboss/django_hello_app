9f2c297e8491781f14fe224444c4aabf907de031
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-04-13 11:33:00 +0600

    Удалено лишнее поле пароля из формы регистрации

--- hello/accounts/forms.py
+++ hello/accounts/forms.py
@@ -3,4 +3,4 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name')
